0e482ec99c61db1e3991d4601ddfac5a9a36833d
Alessandra-Casu <alessandracasu919@gmail.com>
2023-06-27 22:40:58 +0200

    end

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -17,8 +17,19 @@ class PageController extends Controller
   }
 
   public function film() {
-    //chiedere i dati all database
-    $movies = Movie::all();
-    return view ('film', compact('movies'));
+    //chiedere i dati al database
+    //il controller chiama il Model
+    $collMovies = Movie::all(); //SELECT * FROM books
+
+    // $collMovies = Movie::where('title', 'le iene')->get();
+    // $collMovies = Movie::where('title', LIKE, '%bill%')->get();
+
+    //il controller chiama la View passandogli i dati
+    return view ('film', compact('collMovies'));
+    
+           //equivalente a scrivere :
+           //  return view( 'film', [
+            // 'collMovies'=> $movies,
+            // ]);
   }
 }
--- resources/views/film.blade.php
+++ resources/views/film.blade.php
@@ -4,14 +4,18 @@
     <h2>Questi sono i nostri film</h2>
 
     <div class="row g-3 row-cols-3">
-        @foreach ($movies as $movie)
+        @foreach ($collMovies as $movie)
         <div class="col">
          
                 <div class="card h-100">
-                <img src="{{Vite::asset('resources/img/'. $movie->img) }}" class="card-img-top h-75" alt="{{$movie->title}}">
+                {{-- <img src="{{Vite::asset('resources/img/'. $movie->img) }}" class="card-img-top h-75" alt="{{$movie->title}}"> --}}
                 <div class="card-body">
                   <h5 class="card-title">{{$movie->title}}</h5>
-                  <p class="card-text">{{substr($movie->plot, 0, 100) }}..</p>
+                  <h5 class="card-original-title">{{$movie->original_title}}</h5>
+                  <h6 class="card-nationality">{{$movie->nationality}}</h6>
+                  <h6 class="card-date">{{$movie->date}}</h6>
+                  <h6 class="card-date">{{$movie->vote}}</h6>
+                  {{-- <p class="card-text">{{substr($movie->plot, 0, 100) }}..</p> --}}
                   {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
                 </div>
               </div>  
